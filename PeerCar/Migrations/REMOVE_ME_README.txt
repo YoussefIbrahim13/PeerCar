06d00998e89e46d51a0674852e5ca7b57ff97e72
unknown <yy7692651@gmail.com>
2026-06-12 17:13:08 +0200

    fix file

--- PeerCar/Migrations/REMOVE_ME_README.txt
+++
@@ -1,5 +0,0 @@
-Removed duplicate migration files:
-- 20250609152846_Addbookid.cs
-- 20250609152846_Addbookid.Designer.cs
-
-This is to keep migration history clean and avoid schema issues.
--- PeerCar/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PeerCar.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
